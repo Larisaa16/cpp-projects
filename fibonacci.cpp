13517c27b9d7013738684516372df4298cd066d8
Larisaa16 <ciuciularisa@gmail.com>
2024-08-02 23:17:12 +0300

    First commit: Add fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n){
+    if(n<=1){
+        return n;
+    } else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+
+int main(){
+    int numar;
+    cout<<"Poti scrie nr: "; cin>>numar;
+    cout<<"Rezulatul pt "<<numar<<" este: "<<fibonacci(numar);
+    return 0;
+}
